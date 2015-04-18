5861ebca91fc2901812efb44ada46610bf122149
Adrian Brandemuehl <abrandemuehl@gmail.com>
2015-04-17 21:04:16 -0500

    merge in and began work on about page

--- routes/index.js
+++ routes/index.js
@@ -19,44 +19,52 @@ module.exports = function(app, db) {
         var query = [id1, id2];
         query.sort();
         db.collection('onev').find({_id:query.join('-')}).toArray(function(err, result) {
-            console.log(result);
             if(result.length !== 0) {
                 result = result[0];
                 var output = {};
                 var data1 = result[id1];
                 var data2 = result[id2];
                 db.collection("champ_stats").findOne({"_id":parseInt(id1)},function(err,result){
-                var champ1 = result
-                    db.collection("champ_stats").findOne({"_id":parseInt(id2)},function(err,result){
-                        var champ2 = result
-                        output[data.champ1] = champ1
-                        output[data.champ1]["head"] = data1
-                        output[data.champ2] = champ2
-                        output[data.champ2]["head"] = data2
-                        console.log(output)
-                        res.json(output)
-                    })
-                })
+                var champ1 = result;
+                    db.collection("champ_stats").findOne({"_id":parseInt(id2)},function(err,result) {
+                        var champ2 = result;
+                        output[data.champ1] = champ1;
+                        output[data.champ1].head = data1;
+                        output[data.champ2] = champ2;
+                        output[data.champ2].head = data2;
+                        console.log(output);
+                        res.json(output);
+                    });
+                });
                 //res.json(output);
             } else {
                 res.json({err: "No data found"});
             }
         });
     });
-    /* GET home page. */
     app.get('/leaderboard', function(req, res, next) {
         db.collection('champ_stats').find().sort({solo_perc:-1}).limit(10).toArray(function(err, result) {
-            winners = result
+            winners = result;
             db.collection('champ_stats').find().sort({solo_perc:1}).limit(10).toArray(function(err, result) {
-                losers = result
+                losers = result;
                 for (i = 0; i < winners.length; i++) {
-                    winners[i]["champ"] = champs[winners[i]["_id"]]
+                    winners[i].champ = champs[winners[i]._id];
                 }
                 for (i = 0; i < losers.length; i++) {
-                    losers[i]["champ"] = champs[losers[i]["_id"]]
+                    losers[i].champ = champs[losers[i]._id];
                 }
                 res.render('leaderboard', {winners: winners,losers:losers});
+            });
+        });
+    });
 
+    app.get('/about', function(req, res, next) {
+        db.collection('match_ids').count({found:1}, function(err, count) {
+            db.collection('shrooms').aggregate([{
+                $group:{_id:null, shrooms:{$sum:"$shrooms"}}
+            }], function(err, shrooms) {
+                shrooms = shrooms[0].shrooms;
+                res.render("about", {shrooms:shrooms, matches: count});
             });
         });
     });
